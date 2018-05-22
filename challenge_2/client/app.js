694d7a16d5a9f77f87bee735c69a8e644936e232
Jehwa Shin <s.jehwa@gmail.com>
2018-05-22 16:15:20 -0700

    implement client page and added json to csv converter function

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,24 @@
+let App = {
+    init: () => {
+        $('input').click(App.post);
 
+    },
+    post: () => {
+        //send POST ajax request to the server
+        var data = $('textarea').val();
+        // console.log(typeof data);
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:3000/data",
+            data: data,
+            contentType: 'application/json'
+        })
+        // console.log($('textarea').val());
+        // console.log('hi');
+    }
+};
+
+$(document).ready(function() {
+    App.init();
+})
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,47 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+// const converter = require('json-2-csv');
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('./client'));
+app.get('/', (req, res) => {
+    res.sendFile('./client/index.html');
+});
+app.post('/data', (req, res) => {
+    
+    let data = req.body;
+    console.log(converter(data), 'csvDATA==================');
+    res.send('post request done');
+})
 
+let converter = function(obj) {
+    let key = Object.keys(obj).filter( key => key  !== 'children' );
+    //["firstName", "lastName", "county", "city", "role", "sales"]
+    let value = [];
+    let recursion = function(obj) {
+      for(let i = 0; i < key.length; i ++) {
+        value.push(obj[key[i]]);
+      }
+      value.push('\n');
+      if(obj.children) {
+        obj.children.forEach((child) => recursion(child));
+      }
+    }
+    recursion(obj);
+    key.push('\n');
+    let wholeData = key.concat(value);
+    let output = '';
+    for(let i = 0; i < wholeData.length; i ++) {
+        if(wholeData[i] === '\n' || wholeData[i+1] === '\n') {
+            output += wholeData[i];
+        } else {
+            output += wholeData[i] + ',';
+        }
+    }
+    return output;
+  }
+
+app.listen(3000, () => console.log('listening port is 3000'))
