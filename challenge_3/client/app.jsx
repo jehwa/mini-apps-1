2aa5b30225aa4129c6b5e7428b3bf438b987d0d1
Jehwa Shin <s.jehwa@gmail.com>
2018-05-23 15:53:08 -0700

    Implement sending post request to the server

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,95 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            F0: true,
+            F1: false,
+            F2: false,
+            name:'',
+            email:'',
+            passWord:'',
+        }
+    }
 
+    startCheckout(e) {
+        e.preventDefault();
 
+        // console.log('hi');
+        this.setState({
+            F0: false,
+            F1: true,
+        })
+    }
+
+    
+    toF2(e) {
+        let data = {
+            name: this.state.name,
+            email: this.state.email,
+            passWord: this.state.passWord
+        }
+        e.preventDefault();
+        this.setState({
+            F1: false,
+            F2: true
+        })
+        // console.log(data);
+        fetch('http://localhost:3000/f1', {
+            method:'POST', 
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(() => console.log(data))
+        .catch(() => console.log('post request failed'))
+    }
+
+    handleChange(e) {
+       e.preventDefault();
+       this.setState({
+            [e.target.className]: e.target.value,
+        }); 
+    }
+
+    render() {
+        if(this.state.F0) {
+            return (
+              <form onSubmit={(e) => this.startCheckout(e)}>
+                <input type="submit" value="start checkout"/>
+              </form>
+            )
+        }
+        if(this.state.F1) {
+            return (
+                <form onSubmit={(e) => this.toF2(e)}>
+                  <label>
+                      Name:
+                      <input type="text" className="name" onChange={(e) => this.handleChange(e)}/>
+                  </label>
+                  <br></br>
+                  <label>
+                      Email:
+                      <input type="text" className="email" onChange={(e) => this.handleChange(e)}/>
+                  </label>
+                  <br></br>
+                  <label>
+                      Password:
+                      <input type="text" className="passWord" onChange={(e) => this.handleChange(e)}/>
+                  </label>
+                  <br></br>
+                  <input type="submit" value="NEXT"/>
+                
+                </form>
+            )
+        }
+        if(this.state.F2) {
+            return(
+                <div>
+                    this is F2 page
+                </div>
+            )
+        }
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
 
+app.use(express.static('public'));
+// app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+app.post('/f1', (req, res) => {
+    console.log(req.body);
+    res.end();
+})
+
+app.listen(3000, () => console.log('listening on port 3000'))
